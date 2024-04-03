d50b69ef4cfb10865e3dcadc4537af7338e108e6
elianhardyy <elianhardi805@gmail.com>
2024-04-03 14:15:49 +0700

    refactor: update dialect mysql2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
+import mysql2 from "mysql2";
 
 dotenv.config();
 const DATABASE = process.env.RDS_DATABASE!;
@@ -8,8 +9,8 @@ const PASSWORD = process.env.RDS_PASSWORD!;
 const HOST = process.env.RDS_HOSTNAME!;
 const DIALECT : any= process.env.DIALECT;
 
-
 export const database = new Sequelize(DATABASE,USER,PASSWORD,{
     host:HOST,
     dialect:DIALECT,
+    dialectModule:mysql2
 })
