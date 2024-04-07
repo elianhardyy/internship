33cdf23b0d3997c33c3be2a69a2aadacb59c7035
elianhardyy <elianhardi805@gmail.com>
2024-04-07 16:29:04 +0700

    fix: fix cookiieee

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -28,8 +28,8 @@ export const server = () =>{
         cookie:{maxAge:86400000},
     }))
     app.use(express.urlencoded({extended:true}))
+    app.use(express.json())
     app.use(cookieParser())
-    app.use(express.json());
     app.use("/api/v1",router)
     app.get("/",function(req:Request, res:Response){
         res.send(
