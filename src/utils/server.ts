e6e4eb751c1d38636a6524aefd32c6bf8d7444e7
elianhardyy <elianhardi805@gmail.com>
2024-04-07 15:40:42 +0700

    fix: gaeroh

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -28,9 +28,9 @@ export const server = () =>{
         saveUninitialized: true,
         cookie:{maxAge:86400000},
     }))
-    app.use(express.urlencoded({extended:true})),
-    app.use(express.json());
+    app.use(express.urlencoded({extended:true}))
     app.use(cookieParser())
+    app.use(express.json());
     app.use("/api/v1",router)
     app.get("/",function(req:Request, res:Response){
         res.send(
