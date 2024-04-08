72c60315648b92164a10a6c0afcec0b37ee44618
elianhardyy <elianhardi805@gmail.com>
2024-04-08 13:46:16 +0700

    semangat

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -12,49 +12,16 @@ class AuthenticationMiddleware {
                 message:"token not valid"
             })
         }
-        
-       
-        // jwt.verify(token,process.env.SECRET_JWT!,(err:any,verified:any)=>{
-            
-            
-        //     next()
-        // });
-        try {
-            const verified = jwt.verify(token,process.env.SECRET_JWT!)
-            if(verified){
-                req.user = verified
-                const tokenAuth = await Token.findOne({where:{
-                    userId:req.user.id,
-                    token:token,
-                }})
-                if(!tokenAuth){
-                    return res.status(403).json({message:"you must login first"});
-                }
-                else if(token != tokenAuth.token){
-                    return res.status(403).json({message:"your token not verified"})
-                }
-                else if(tokenAuth!!.expires_at <= new Date(Date.now())){
-                    await Token.destroy({where:{userId:req.user.id}})
-                    return res.status(403).json({message:"you can't login"})
-                }else{
-                    // if(userblacklist){    
-                    //     return res.status(403).json({message:"you have been logged out"});
-                    // }
-                    next();
-                    
-                }
-            }else{
-                 return res.status(403).json({message:'bad request'})
+        jwt.verify(token,process.env.SECRET_JWT!,(err:any,verified:any)=>{
+            if(err){
+                return res.status(401).send({
+                    message:"Unauthorized",
+                    err
+                })
             }
-        } catch (error) {
-            
-            return res.status(401).send({
-                message:"Unauthorized",
-                error
-            })
-            
-        }
-            // console.log(req.session.authenticated)
+            req.user = verified
+            next()
+        });
             // if(req.session.authenticated){
                 
             // }else{
@@ -62,7 +29,26 @@ class AuthenticationMiddleware {
             // }  
         // await Blacklist.destroy({where:{userId:req.user.id}})   
         //const userblacklist = await Blacklist.findOne({where:{userId:req.user.id}}) 
-        
+        const tokenAuth = await Token.findOne({where:{
+            userId:req.user.id,
+            token:token,
+        }})
+        if(!tokenAuth){
+            return res.status(403).json({message:"you must login first"});
+        }
+        else if(token != tokenAuth.token){
+            return res.status(403).json({message:"your token not verified"})
+        }
+        else if(tokenAuth!!.expires_at <= new Date(Date.now())){
+            await Token.destroy({where:{userId:req.user.id}})
+            return res.status(403).json({message:"you can't login"})
+        }else{
+            // if(userblacklist){    
+            //     return res.status(403).json({message:"you have been logged out"});
+            // }
+            next();
+            
+        }
     }
     public async validation(req: Request | any, res:Response, next:NextFunction) : Promise<Response<any,Record<string,any>> | undefined> {
         const errors = validationResult(req)
